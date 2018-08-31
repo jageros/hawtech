6c33b9199a38447a121a81e5ca8dbe92df8e3a63
jay <lhj168os@gmail.com>
2018-08-31 18:21:36 +0800

    create project

---
+++ controllers/default.go
@@ -0,0 +1,53 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type IndexController struct {
+	beego.Controller
+}
+
+type AboutController struct {
+	beego.Controller
+}
+
+type CaseController struct {
+	beego.Controller
+}
+
+type NewsController struct {
+	beego.Controller
+}
+
+type NewsDeyailController struct {
+	beego.Controller
+}
+
+type ProductController struct {
+	beego.Controller
+}
+
+func (c *IndexController) Get() {
+	c.TplName = "index.html"
+}
+
+func (c *AboutController) Get() {
+	c.TplName = "about.html"
+}
+
+func (c *CaseController) Get() {
+	c.TplName = "case.html"
+}
+
+func (c *NewsController) Get() {
+	c.TplName = "news.html"
+}
+
+func (c *NewsDeyailController) Get() {
+	c.TplName = "newsDetail.html"
+}
+
+func (c *ProductController) Get() {
+	c.TplName = "product.html"
+}
---
+++ routers/router.go
@@ -0,0 +1,17 @@
+package routers
+
+import (
+	"hawtech/controllers"
+
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	beego.Router("/", &controllers.IndexController{})
+	beego.Router("/index", &controllers.IndexController{})
+	beego.Router("/about", &controllers.AboutController{})
+	beego.Router("/case", &controllers.CaseController{})
+	beego.Router("/news", &controllers.NewsController{})
+	beego.Router("/newsDetail", &controllers.NewsDeyailController{})
+	beego.Router("/product", &controllers.ProductController{})
+}
